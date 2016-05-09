8ff931ad3fe3b3a06401055774ed5cd7c9906352
Ashutosh Dixit <ashutosh.dixit@intel.com>
2016-05-09 15:42:38 -0400

    IB/hfi1: Change hfi1_init loop to preserve error returns
    If one iteration of the loop causes an error return and a later iteration
    doesn't, the later iteration causes the earlier error condition to be
    lost. This could result in driver probe succeeding when it should have
    failed. Therefore save off the error return in the loop itself rather than
    outside the loop.
    
    Reviewed-by: Dennis Dalessandro <dennis.dalessandro@intel.com>
    Reviewed-by: Mitko Haralanov <mitko.haralanov@intel.com>
    Signed-off-by: Ashutosh Dixit <ashutosh.dixit@intel.com>

--- init.c
+++ init.c
@@ -737,12 +737,14 @@ int hfi1_init(struct hfi1_devdata *dd, int reinit)
 		lastfail = hfi1_create_rcvhdrq(dd, rcd);
 		if (!lastfail)
 			lastfail = hfi1_setup_eagerbufs(rcd);
-		if (lastfail)
+		if (lastfail) {
 			dd_dev_err(dd,
 				   "failed to allocate kernel ctxt's rcvhdrq and/or egr bufs\n");
+			ret = lastfail;
+		}
 	}
-	if (lastfail)
-		ret = lastfail;
+	if (ret)
+		goto done;
 
 	/* Allocate enough memory for user event notification. */
 	len = ALIGN(dd->chip_rcv_contexts * HFI1_MAX_SHARED_CTXTS *
